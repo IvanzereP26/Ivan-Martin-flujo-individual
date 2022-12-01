4644a894e05f69d6f9468ff4b153add110c57b94
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-12-01 19:45:14 +0100

    Segundo Commit

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
-public class HolaMundo {
-    public static void main(String args[]){
-        System.out.println("Hola Mundo");
+public class Mi_Clase_1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hola, que tal?
+Hoy gana España a Japón y pasamos como primeros de grupo
